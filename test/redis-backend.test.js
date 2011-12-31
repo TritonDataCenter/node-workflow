2f6338620401cf8d43fabec0709b26060901ae89
Pedro P. Candel <kusorbox@gmail.com>
2011-12-31 11:10:43 +0100

    Redis backend runJob method

--- test/redis-backend.test.js
+++ test/redis-backend.test.js
@@ -10,6 +10,8 @@ var backend, factory;
 
 var aTask, aWorkflow, aJob, anotherTask, fallbackTask;
 
+var runnerId = uuid();
+
 // A DB for testing, flushed before and right after we're done with tests
 var TEST_DB_NUM = 15;
 
@@ -248,6 +250,26 @@ test('next queued job', function(t) {
     });
   });
 });
+
+
+test('run job', function(t) {
+  backend.runJob(aJob.uuid, runnerId, function(err) {
+    t.ifError(err, 'run job error');
+    // If the job is running, it shouldn't be available for nextJob:
+    backend.nextJob(function(err, job) {
+      t.ifError(err, 'run job next error');
+      t.notEqual(aJob.uuid, job.uuid);
+      backend.getJob(aJob.uuid, function(err, job) {
+        t.ifError(err, 'run job getJob');
+        t.equal(job.runner, runnerId);
+        t.equal(job.status, 'running');
+        aJob = job;
+        t.end();
+      });
+    });
+  });
+});
+
 
 test('teardown', function(t) {
   // None of the deleteWorkflow/deleteTask calls are needed since we are
