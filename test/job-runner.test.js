685e54a0417bc6ff7f6aa176612d8136f82a7b1d
Pedro P. Candel <kusorbox@gmail.com>
2012-02-10 18:15:44 +0100

    Closes GH-21: Job cancelation implemented.

--- test/job-runner.test.js
+++ test/job-runner.test.js
@@ -27,6 +27,7 @@ var okWf, failWf, timeoutWf, reQueueWf, reQueuedJob, elapsed;
 var FakeRunner = function() {
   this.child_processes = {};
   this.uuid = uuid();
+  this.run_interval = 1000;
 };
 
 FakeRunner.prototype.childUp = function(job_uuid, child_pid) {
@@ -563,6 +564,72 @@ test('a job can access explicitly defined sandbox modules', function(t) {
   });
 
 });
+
+
+test('a canceled job', function(t) {
+  factory.workflow({
+    name: 'Job will be canceled',
+    chain: [{
+      name: 'Timeout Task',
+      timeout: 2,
+      body: function(job, cb) {
+        setTimeout(function() {
+          // Should not be called:
+          return cb('Error within timeout');
+        }, 3050);
+      },
+      retry: 3
+    }],
+    onerror: [{
+      name: 'A name',
+      body: function(job, cb) {
+        return cb('It should not run');
+      }
+    }]
+  }, function(err, wf) {
+    t.ifError(err, 'wf error');
+    t.ok(wf, 'wf ok');
+    factory.job({
+      workflow: wf.uuid,
+      exec_after: '2012-01-03T12:54:05.788Z'
+    }, function(err, job) {
+      t.ifError(err, 'job error');
+      t.ok(job, 'job ok');
+      wf_job_runner = new WorkflowJobRunner({
+        runner: runner,
+        backend: backend,
+        job: job,
+        trace: false
+      });
+      t.ok(wf_job_runner, 'wf_job_runner ok');
+      backend.runJob(job.uuid, runner.uuid, function(err) {
+        t.ifError(err, 'backend.runJob error');
+        setTimeout(function() {
+          backend.updateJobProperty(
+            job.uuid,
+            'execution',
+            'canceled',
+            function(err) {
+              t.ifError(err, 'updateJobProperty error');
+            });
+        }, 750);
+        wf_job_runner.run(function(err) {
+          t.ifError(err, 'wf_job_runner run error');
+          backend.getJob(job.uuid, function(err, job) {
+            t.ifError(err, 'get job error');
+            t.ok(job, 'get job ok');
+            t.equal(job.execution, 'canceled');
+            t.ok(job.chain_results[0].error, 'task fails');
+            t.equal(job.onerror_results.length, 0, 'should not call onerror');
+            t.end();
+          });
+        });
+      });
+    });
+  });
+
+});
+
 
 test('teardown', function(t) {
   backend.quit(function() {
