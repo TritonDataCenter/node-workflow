885adbff7dd7e7b72635f8983f6a8bd832114a54
Pedro P. Candel <kusorbox@gmail.com>
2012-03-08 12:42:48 +0100

    GH-54: Error Objects based on restify.RestError

---
+++ test/errors.test.js
@@ -0,0 +1,34 @@
+var test = require('tap').test,
+    util = require('util'),
+    restify = require('restify'),
+    wf = require('../lib/index');
+
+
+test('errors defined', function (t) {
+  t.equal(typeof (wf.BackendError), 'function');
+  t.equal(typeof (wf.BackendInternalError), 'function');
+  t.equal(typeof (wf.BackendInvalidArgumentError), 'function');
+  t.equal(typeof (wf.BackendMissingParameterError), 'function');
+  t.equal(typeof (wf.BackendPreconditionFailedError), 'function');
+  t.equal(typeof (wf.BackendResourceNotFoundError), 'function');
+  t.end();
+});
+
+test('errors to RestErrors', function (t) {
+  var nfError, nfRestError, errors = [
+    wf.BackendInternalError,
+    wf.BackendInvalidArgumentError,
+    wf.BackendMissingParameterError,
+    wf.BackendPreconditionFailedError,
+    wf.BackendResourceNotFoundError
+  ];
+  errors.forEach(function (E) {
+    nfError = new E('A message');
+    t.equal(typeof (nfError), 'object');
+    nfRestError = nfError.toRestError;
+    t.equal(typeof (nfRestError), 'object');
+    t.equal(nfError.statusCode, nfRestError.statusCode);
+    t.equal(nfError.message, nfRestError.message);
+  });
+  t.end();
+});
